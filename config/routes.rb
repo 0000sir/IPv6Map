837d93cd0cc5bcabd56cad484116aaa98bcab097
0000sir <0000sir@gmail.com>
2019-04-19 13:32:03 +0800

    clustered map

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'map/index'
+  get 'map/index/(:v)' => 'map#index'
+  get 'map/no_ipv6'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'map#index'
 end
